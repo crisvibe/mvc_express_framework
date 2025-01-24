a87387c750d7a21a942a4c0134880d12a2b313a6
Cris a <devcris1@outlook.com>
2025-01-24 22:17:38 +0100

    remove directory

--- backup_i01/controller/usersController.js
+++
@@ -1,8 +0,0 @@
-const { getAllUsers } = require('../model/usersModel');
-
-const renderAllUsers = async (req, res) => {
-  const users = await getAllUsers();
-  res.render('users', { users });
-}
-
-module.exports = { renderAllUsers };
--- backup_i01/model/usersModel.js
+++
@@ -1,8 +0,0 @@
-const users = ['Criss', 'Eileen', 'Gherman'];
-
-// Export the getAllUsers function. Retuns the data
-const getAllUsers = () => {
-  return users;
-}
-
-module.exports = { getAllUsers };
